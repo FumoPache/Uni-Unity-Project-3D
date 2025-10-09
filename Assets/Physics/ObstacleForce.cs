ae9c0ec13098639627f9307a7d9c612d0b6a15d4
FumoPache <ryanmorrice1@gmail.com>
2025-10-09 20:22:03 +0100

    Physics added
    Will need to check with Verda that this what they mean under the coursework guideline

---
+++ Assets/Physics/ObstacleForce.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class ObstacleForce : MonoBehaviour
+{
+    [SerializeField]    
+    private float forceMagnitude;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        Rigidbody rigidbody = hit.collider.attachedRigidbody;
+
+        if (rigidbody != null)
+        { 
+        Vector3 forceDirection = hit.gameObject.transform.position - transform.position;    
+            forceDirection.y = 0;
+            forceDirection.Normalize();
+
+            rigidbody.AddForceAtPosition(forceDirection * forceMagnitude, transform.position, ForceMode.Impulse);
+
+        }
+
+    }
+}
